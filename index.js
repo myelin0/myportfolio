9b15745341add60a28aeedb6922b4081327129cb
Emi <emi.veisllari@gmail.com>
2021-12-14 16:35:07 +0100

    Add events for Mobile Menu

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const hamburger = document.querySelector('.menu-button');
 const closebtn = document.querySelector('.closebtn');
 const navmenu = document.querySelector('.menu-links');
+const navportal = document.querySelector('.po');
+const navabout = document.querySelector('.ab');
+const navcontact = document.querySelector('.co');
 
 hamburger.addEventListener('click', () => {
   navmenu.classList.toggle('show');
 });
 
 closebtn.addEventListener('click', () => {
+  navmenu.style.display = 'none';
+});
+
+navportal.addEventListener('click', () => {
+  navmenu.style.display = 'none';
+});
+
+navabout.addEventListener('click', () => {
+  navmenu.style.display = 'none';
+});
+
+navcontact.addEventListener('click', () => {
   navmenu.style.display = 'none';
 });
