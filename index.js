8aa5fd56f657930b4ed20c44f82f59b13d2415ae
Devtiwo <dontiwo5020@yahoo.com>
2021-12-17 09:28:39 +0100

    Add variables for form validation

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ for (let i = 0; i < link.length; i += 1) {
     navmenu.classList.add('invisible');
   });
 }
+
+const email = document.getElementById('email');
+const errorMessage = document.getElementById('error');
+const form = document.getElementById('form');
+const isLowerCase = (str) => /[a-z]/.test(str) && !/[A-Z]/.test(str);
